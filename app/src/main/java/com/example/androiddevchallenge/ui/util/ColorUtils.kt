070790db7aacca8a1c89d22980220a92c68d337b
Vivek Sharma <viveksharma2382000@gmail.com>
2021-02-28 01:00:38 +0530

    Fixed build errors

--- app/src/main/java/com/example/androiddevchallenge/ui/util/ColorUtils.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/util/ColorUtils.kt
@@ -1,7 +1,12 @@
 package com.example.androiddevchallenge.ui.util
 
 import androidx.compose.ui.graphics.Color
-import com.example.androiddevchallenge.ui.theme.*
+import com.example.androiddevchallenge.ui.theme.blue
+import com.example.androiddevchallenge.ui.theme.female
+import com.example.androiddevchallenge.ui.theme.green
+import com.example.androiddevchallenge.ui.theme.male
+import com.example.androiddevchallenge.ui.theme.orange
+import com.example.androiddevchallenge.ui.theme.red
 
 fun getBackgroundColor(): Color {
     val colorsList = listOf(red, green, blue, orange)
